51679cd6f5ecba61889a95f43030efad244a605f
YI <ilovey_hwy@163.com>
2018-08-14 11:01:41 +0800

    spring-boot-webflux-websocket 初始化

---
+++ spring-boot-webflux-websocket/src/main/java/com/example/webflux/SpringBootWebfluxWebsocketApplication.java
@@ -0,0 +1,12 @@
+package com.example.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebfluxWebsocketApplication.class, args);
+    }
+}
