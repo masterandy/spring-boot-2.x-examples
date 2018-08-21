21795391a12382c7d7c2de7b0318641a7ceb545f
YI <ilovey_hwy@163.com>
2018-08-21 10:58:38 +0800

    统一异常处理

---
+++ spring-boot-exception/src/main/java/com/yi/exception/handler/MyException.java
@@ -0,0 +1,14 @@
+package com.yi.exception.handler;
+
+/**
+ * 统一异常
+ * @author YI
+ * @date 2018-8-21 10:48:31
+ */
+public class MyException extends Exception {
+
+    public MyException(String message) {
+        super(message);
+    }
+
+}
