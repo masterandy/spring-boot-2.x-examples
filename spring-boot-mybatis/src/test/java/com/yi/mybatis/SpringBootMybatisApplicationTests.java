d6f61f2f500e6cc6ef27ca3d7ee930bcc9235098
YI <ilovey_hwy@163.com>
2018-08-23 18:11:54 +0800

    更新mybatis测试用例和文档

--- spring-boot-mybatis/src/test/java/com/yi/mybatis/SpringBootMybatisApplicationTests.java
+++ spring-boot-mybatis/src/test/java/com/yi/mybatis/SpringBootMybatisApplicationTests.java
@@ -2,6 +2,7 @@ package com.yi.mybatis;
 
 import cn.hutool.json.JSONUtil;
 import com.yi.mybatis.model.Baike;
+import com.yi.mybatis.model.BaikeExample;
 import com.yi.mybatis.service.BaikeService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +19,9 @@ public class SpringBootMybatisApplicationTests {
     @Autowired
     BaikeService baikeService;
 
+    /**
+     * 添加数据
+     */
     @Test
     public void test1() {
         List<String> list1 = new ArrayList<>();
@@ -34,6 +38,68 @@ public class SpringBootMybatisApplicationTests {
 
         baikeService.insertSelective(baike1);
         baikeService.insertSelective(baike2);
+    }
+
+    /**
+     * 查询数据
+     */
+    @Test
+    public void test2() {
+        BaikeExample example = new BaikeExample();
+        BaikeExample.Criteria criteria = example.createCriteria();
+
+        criteria.andNameEqualTo("海明威");
+
+        List<Baike> baikes = baikeService.selectByExample(example);
+
+        System.out.println(baikes);
+    }
+
+    /**
+     * 查询数据
+     */
+    @Test
+    public void test3() {
+        BaikeExample example = new BaikeExample();
+        BaikeExample.Criteria criteria = example.createCriteria();
+
+        // 查询点赞数大于1000的数据
+        criteria.andGoodGreaterThan(1000);
+
+        List<Baike> baikes = baikeService.selectByExample(example);
+
+        System.out.println(baikes);
+    }
+
+    /**
+     * 更新数据
+     */
+    @Test
+    public void test4() {
+        Baike baike = new Baike();
+        baike.setId(1L);
+        baike.setGood(1001);
+
+        // 不更新为null的字段
+        int i = baikeService.updateByPrimaryKeySelective(baike);
+
+        System.out.println("更新 " + i + " 条数据");
+    }
+
+    /**
+     * 更新数据
+     */
+    @Test
+    public void test5() {
+        BaikeExample example = new BaikeExample();
+        BaikeExample.Criteria criteria = example.createCriteria();
+
+        criteria.andBadBetween(5, 10);
+
+        // 按条件删除（删除鄙视数5到10的数据）
+        int i = baikeService.deleteByExample(example);
+
+        System.out.println("更新 " + i + " 条数据");
     }
 
 }
