b89cc5c954f8c4355ae88e178b6d290dfa80e451
YI <ilovey_hwy@163.com>
2018-11-06 17:54:55 +0800

    更新定时任务

--- spring-boot-scheduled/src/main/java/com/yi/scheduled/task/ScheduledTasks.java
+++ spring-boot-scheduled/src/main/java/com/yi/scheduled/task/ScheduledTasks.java
@@ -26,4 +26,12 @@ public class ScheduledTasks {
     public void reportCurrentTime() {
         log.info("现在时间是 {}", dateFormat.format(new Date()));
     }
+
+    /**
+     * 2秒钟执行一次
+     */
+    @Scheduled(cron = "0/2 * * * * ?")
+    public void cronTask() {
+        log.info("cron 现在时间是 {}", dateFormat.format(new Date()));
+    }
 }
