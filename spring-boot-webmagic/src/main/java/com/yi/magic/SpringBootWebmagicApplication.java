e9932948f01370d3fab88404e2cbc38a776e1125
YI <ilovey_hwy@163.com>
2018-11-23 17:53:59 +0800

    爬虫

---
+++ spring-boot-webmagic/src/main/java/com/yi/magic/SpringBootWebmagicApplication.java
@@ -0,0 +1,12 @@
+package com.yi.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebmagicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebmagicApplication.class, args);
+    }
+}
