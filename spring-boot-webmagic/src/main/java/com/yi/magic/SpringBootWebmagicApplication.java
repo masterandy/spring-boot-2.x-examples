92745a6595801554d97aeb1d9bc722252fc843f9
YI <ilovey_hwy@163.com>
2018-11-28 10:52:58 +0800

    爬虫注释

--- spring-boot-webmagic/src/main/java/com/yi/magic/SpringBootWebmagicApplication.java
+++ spring-boot-webmagic/src/main/java/com/yi/magic/SpringBootWebmagicApplication.java
@@ -3,6 +3,11 @@ package com.yi.magic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 爬虫
+ * @author YI
+ * @date 2018-11-27 10:50:30
+ */
 @SpringBootApplication
 public class SpringBootWebmagicApplication {
 
