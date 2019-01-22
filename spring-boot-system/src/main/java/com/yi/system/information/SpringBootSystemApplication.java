789ff4f2d8233da03d5c29d094979c8b3fea11c9
hwy <ilovey_hwy@163.com>
2019-01-22 15:39:16 +0800

    系统监控

---
+++ spring-boot-system/src/main/java/com/yi/system/information/SpringBootSystemApplication.java
@@ -0,0 +1,14 @@
+package com.yi.system.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSystemApplication.class, args);
+    }
+
+}
+
