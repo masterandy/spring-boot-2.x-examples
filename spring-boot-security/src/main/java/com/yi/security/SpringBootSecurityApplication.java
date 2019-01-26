3cdd6d0be40dcdaf1d913c3ad274ba9acc5dc7c5
hwy <ilovey_hwy@163.com>
2019-01-26 16:01:47 +0800

    权限框架基础操作

--- spring-boot-security/src/main/java/com/yi/security/SpringBootSecurityApplication.java
+++ spring-boot-security/src/main/java/com/yi/security/SpringBootSecurityApplication.java
@@ -3,6 +3,11 @@ package com.yi.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Security安全
+ * @author YI
+ * @date 2019-1-26 15:48:10
+ */
 @SpringBootApplication
 public class SpringBootSecurityApplication {
 
