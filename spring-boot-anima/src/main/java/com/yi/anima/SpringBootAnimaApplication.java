181b01359c04bf9704703148119e0597a3e43677
hwy <ilovey_hwy@163.com>
2019-01-29 09:21:22 +0800

    # 注释

--- spring-boot-anima/src/main/java/com/yi/anima/SpringBootAnimaApplication.java
+++ spring-boot-anima/src/main/java/com/yi/anima/SpringBootAnimaApplication.java
@@ -3,6 +3,12 @@ package com.yi.anima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Anima数据库操作框架
+ *
+ * @author YI
+ * @date 2019-1-29 09:21:04
+ */
 @SpringBootApplication
 public class SpringBootAnimaApplication {
 
