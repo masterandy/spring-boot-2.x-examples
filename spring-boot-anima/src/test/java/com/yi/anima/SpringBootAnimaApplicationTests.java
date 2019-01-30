f2a9cbf451aab3ccc4392236f07e663f32ec61ff
hwy <ilovey_hwy@163.com>
2019-01-30 11:33:06 +0800

    #

--- spring-boot-anima/src/test/java/com/yi/anima/SpringBootAnimaApplicationTests.java
+++ spring-boot-anima/src/test/java/com/yi/anima/SpringBootAnimaApplicationTests.java
@@ -24,6 +24,7 @@ public class SpringBootAnimaApplicationTests {
 
     @Test
     public void test2() {
+        // 查询所有数据
         List<Baike> baikes = baiKeService.selectListAll();
         System.out.println(baikes);
     }
