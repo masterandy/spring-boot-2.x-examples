dc95f364acfbe2f43eca7cabb91db20c9460ecbe
hwy <ilovey_hwy@163.com>
2019-03-15 23:31:19 +0800

    #

--- spring-boot-system/src/main/java/com/yi/system/information/SpringBootSystemApplication.java
+++ spring-boot-system/src/main/java/com/yi/system/information/SpringBootSystemApplication.java
@@ -3,6 +3,11 @@ package com.yi.system.information;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 系统信息
+ * @author YI
+ * @date 2019-3-14 22:56:43
+ */
 @SpringBootApplication
 public class SpringBootSystemApplication {
 
